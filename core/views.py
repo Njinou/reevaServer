04cc69b513f648d2406236f4fa430b6a5571b904
Pascal Nitcheu <nitcheupascal@gmail.com>
2025-01-27 16:06:03 +0100

    define ViewSet classes to handle API endpoints for Project and Task

--- core/views.py
+++ core/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Project, Task
+from .serializers import ProjectSerializer, TaskSerializer
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Restrict projects to the ones owned by the current user
+        return self.queryset.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        # Automatically set the owner to the logged-in user
+        serializer.save(owner=self.request.user)
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Restrict tasks to projects owned by the current user
+        return self.queryset.filter(project__owner=self.request.user)
